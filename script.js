ac21d730f7821f1727bf916b7d80e735d39b0cfc
rlw92 <r_l_williams@yahoo.com>
2024-05-10 16:48:55 +0100

    Added questlog.

--- script.js
+++ script.js
@@ -13,5 +13,32 @@ function toggleNavMenu(){
 nvBtn.addEventListener("click", toggleNavMenu);
 clsBtn.addEventListener("click", toggleNavMenu);
 
+//Modal Functionality
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.querySelectorAll("")
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeMdl")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
